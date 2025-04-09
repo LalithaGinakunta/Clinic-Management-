6ed8a90141ad380d195b70fb8c352ecb2932de9f
LalithaGinakunta <lalithaginakunta612@gmail.com>
2025-04-09 20:03:09 +0530

    layout.tsx

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import type React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
+import "../globals.css"
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
